0b54c6d4f87ed33109956abac333ccf772336721
jklama <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>
2024-04-18 07:03:49 +0545

    not working tailwind so i will check it aftersome time and 38

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,22 +1,31 @@
 import { StyleSheet, Text, View } from "react-native";
-import { Slot } from "expo-router";
-import React from "react";
+import { Slot, Stack, SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+import React, { useEffect } from "react";
 
+SplashScreen.preventAutoHideAsync();
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+  if (!fontsLoaded && !error) return null;
   return (
-    <View style={styles.container}>
-      <Text>RootLayout</Text>
-    </View>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
   );
 };
 
 export default RootLayout;
-
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
+import { Link } from "expo-router";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-5xl font-pbold">Aora</Text>
+      <Text className=" text-xs font-bold">Yo this is test</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
